637a72b090cff0753157ad7ca93d53230faf49dd
vjp888 <rellik888@gmail.com>
2018-10-31 09:36:17 -0600

    Adds working add artsit method and tests

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -17,6 +17,18 @@ class CuratorTest < MiniTest::Test
                 artist_id: "2",
                 year: "1941"
                 }
+    @artist_1 = {id: "1",
+                name: "Henri Cartier-Bresson",
+                born: "1908",
+                died: "2004",
+                country: "France"
+                }
+    @artist_2 = {id: "2",
+                name: "Ansel Adams",
+                born: "1902",
+                died: "1984",
+                country: "United States"
+                }
   end
 
   def test_it_exists
@@ -35,12 +47,19 @@ class CuratorTest < MiniTest::Test
   def test_it_can_add_photos
     curator = Curator.new
     curator.add_photograph(@photo_1)
-
     assert_equal [@photo_1], curator.photographs
 
     curator.add_photograph(@photo_2)
-    
     assert_equal [@photo_1, @photo_2], curator.photographs
   end
 
+  def test_it_can_add_artists
+    curator = Curator.new
+
+    curator.add_artist(@artist_1)
+    assert_equal [@artist_1], curator.artists
+
+    curator.add_artist(@artist_2)
+    assert_equal [@artist_1, @artist_2], curator.artists
+  end
 end
